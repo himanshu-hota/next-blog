6862ca8f0d93adc67ea1e950c325cf3ffd346521
Himanshu Hota <62291769+himanshu-hota@users.noreply.github.com>
2023-03-16 13:29:23 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    env:{
-        mongodb_username: 'next-user',
-        mongodb_password: 'P1d97HUqsARreg98',
-        mongodb_clusterName: 'cluster0',
-        mongodb_database: 'my-site',
-    }
-}
